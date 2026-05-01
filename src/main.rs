f90f2afabe70d8afb031dc7b2b380af7572e73dd
Liam Turelid <Turelid@chalmers.se>
2026-05-02 01:37:50 +0200

    ugly as hell but kinda working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::time::{Duration, SystemTime};
 //use std::thread::sleep;
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use crossterm::{
     execute,
     event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
@@ -17,10 +17,11 @@ struct position {
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
     execute!(stdout(), Clear(ClearType::All)).unwrap();
-    let position = position{
-        x: size()?.0,
-        y: size()?.1,
-    };
+    let (width, height) = size()?;
+    let position = position {
+        x: width,
+        y: height,
+    };    
     execute!(stdout(), MoveTo(position.x / 4, position.y / 4))?;
     println!("Hello, world!");
     let now = SystemTime::now();
@@ -40,9 +41,10 @@ fn main() -> io::Result<()> {
     };
  
     match key.code {
-        KeyCode::Char(c) => println!("Key: '{}' (Modifiers: {})", c, modifiers),
-        code => println!("Key: {:?} (Modifiers: {})", code, modifiers),
+        KeyCode::Char(c) => print!("Key: '{}' (Modifiers: {})", c, modifiers),
+        code => print!("Key: {:?} (Modifiers: {})", code, modifiers),
         }
+        stdout().flush();
     }
     defer! {
         let _ = disable_raw_mode();
@@ -50,7 +52,8 @@ fn main() -> io::Result<()> {
     loop {
         if score == 6
         {
-            println!("You are done!");
+            print!("You are done!");
+            stdout().flush()?;
             break;
         }
         if event::poll(std::time::Duration::from_millis(50))? {
@@ -59,11 +62,13 @@ fn main() -> io::Result<()> {
                     // Quit on Ctrl+C
                     if key.modifiers.contains(KeyModifiers::CONTROL) && key.code == KeyCode::Char('c') {
                         println!("\nCtrl+C pressed. Quitting...");
+                        stdout().flush()?;
                         break;
                     }
                     print_key_event(key);
                     if key.code == KeyCode::Char(test[score]) {
-                        println!("that's correct!");
+                        print!("that's correct!");
+                        stdout().flush()?;
                         score += 1;
                     }
                 }
